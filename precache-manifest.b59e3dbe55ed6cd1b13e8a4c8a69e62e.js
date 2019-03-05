eb44a3ed9c90aa7ea4020991533e5ee3c99f5748
Cian O'Ruanaidh <cianor.social@gmail.com>
2019-03-05 09:49:07 -0500

    Updates

---
+++ precache-manifest.b59e3dbe55ed6cd1b13e8a4c8a69e62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac1afb50e6cd074d941",
+    "url": "/a-single-word-v2/static/css/main.6173dd89.chunk.css"
+  },
+  {
+    "revision": "2ac1afb50e6cd074d941",
+    "url": "/a-single-word-v2/static/js/main.2ac1afb5.chunk.js"
+  },
+  {
+    "revision": "cc3c2400ab0a5fcf4df9",
+    "url": "/a-single-word-v2/static/js/runtime~main.cc3c2400.js"
+  },
+  {
+    "revision": "698dfe8ce10941f92c24",
+    "url": "/a-single-word-v2/static/js/2.698dfe8c.chunk.js"
+  },
+  {
+    "revision": "13634846fdbc1d714ec6c905cfd4fd07",
+    "url": "/a-single-word-v2/static/media/debby-hudson-607042-unsplash.13634846.jpg"
+  },
+  {
+    "revision": "d65ff5ed174cc1856541ff3bc53475df",
+    "url": "/a-single-word-v2/index.html"
+  }
+];
